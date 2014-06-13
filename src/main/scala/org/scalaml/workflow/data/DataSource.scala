83ed9c477f12fd8016d3506a44c52dc5bb27268f
prnicolas <pnicolas57@yahoo.com>
2014-06-12 23:58:25 -0700

    Update the Spark related files and create scalabilty package

--- src/main/scala/org/scalaml/workflow/data/DataSource.scala
+++ src/main/scala/org/scalaml/workflow/data/DataSource.scala
@@ -13,13 +13,28 @@ import org.scalaml.stats.Stats
 import scala.Array.canBuildFrom
 import org.scalaml.workflow.PipeOperator
 import scala.io.Source
+import org.scalaml.core.Types.ScalaMl._
 
 		/**
-		 * Generic class to load or save files into either HDFS or local files system
+		 * <p>Generic class to load or save files into either HDFS or local files system.</p>
+		 * @param pathName relative path for the data files
+		 * @param normalize flag to normalize data within the range [0,1]
+		 * @param reverseOrder flag to re-order/index the data from the last entry to the first entry
+		 * @param headerLines number of header lines in the file
+		 * @exception IllegalArgumentException if the path name is undefined
+		 * 
+		 * @author Patrick Nicolas
+		 * @date December 11, 2013
+		 * @project Scala for Machine Learnin
 		 */
 class DataSource(val pathName: String, 
 				 val normalize: Boolean, 
-				 val reverseOrder: Boolean) extends PipeOperator[List[Array[String] =>Double], List[XTSeries[Double]]] {
+				 val reverseOrder: Boolean,
+				 val headerLines: Int = 1) extends PipeOperator[List[Array[String] =>Double], List[XTSeries[Double]]] {
+	
+   require(pathName != null && pathName.length > 1, "Cannot create a data source with undefined path")
+   require(headerLines >=0,  "Cannot create a data source with negative number of lines for header " + headerLines)
+   
    import scala.io.Source
    
    final val CSV_DELIM = ","
@@ -30,7 +45,7 @@ class DataSource(val pathName: String,
    
   	  val file = new File(pathName)
   	  if( file.isDirectory)
-         file.listFiles.map( x => x.getName)
+         file.listFiles.map( _.getName)
       else
          Array[String](pathName)
    }
@@ -40,11 +55,10 @@ class DataSource(val pathName: String,
 		   */
    private def load: Option[(TxtFields, Array[TxtFields])] = {
      import java.io.{IOException, FileNotFoundException}
-     require ( pathName != null & pathName.length > 1, "Cannot load data from undefined path")
      
      try {
     	 val src = Source.fromFile(pathName)
-	  	 val fields = src.getLines.map( _.split(CSV_DELIM)).toArray.drop(1)
+	  	 val fields = src.getLines.map( _.split(CSV_DELIM)).toArray.drop(headerLines)
 	  	 val results = if( reverseOrder ) fields.reverse else  fields
 	  	 val textFields = (fields(0), results)
 	  	 src.close
@@ -73,8 +87,10 @@ class DataSource(val pathName: String,
    }
    			/**
    			 * Load, extracts, convert and normalize a list of fields using an extractors.
+   			 * @param ext function to extract and convert a list of comma delimited fields into a double
    			 * @return list of array of values or an empty list if either the input file is not found or the extraction and
    			 * conversion of some of the text fields failed.
+   			 * @exception IllegalArgumentException if the extraction function is undefined
    			 */
    override def |> (extr: List[TxtFields =>Double]): Option[List[XTSeries[Double]]] = {
      require(extr != null && extr.size > 0, "Cannot extracts fields with undefined extractors")
@@ -108,6 +124,21 @@ class DataSource(val pathName: String,
        case None => None
      }
    }
+   
+   
+  def load(extr: TxtFields => DblVector): Option[XTSeries[DblVector]] = {
+     require(extr != null, "Cannot extracts fields with undefined extractors")
+  	 
+  	 load match {
+       case Some(data) => {
+          if( normalize) 
+          	  XTSeries.normalize(XTSeries[DblVector](data._2.map( extr(_))))
+          else 
+             Some(XTSeries[DblVector](data._2.map( extr(_))))
+       }
+       case None => None
+     }
+   }
 }
 
 
@@ -119,7 +150,7 @@ object DataSource {
         directory.listFiles.map( _.getName)
     }
   
-  def apply(pathName: String, normalize: Boolean, reverseOrder:Boolean): DataSource = new DataSource(pathName, normalize, reverseOrder)
+  def apply(pathName: String, normalize: Boolean, reverseOrder:Boolean, headerLines: Int): DataSource = new DataSource(pathName, normalize, reverseOrder, headerLines)
   def apply(pathName: String, normalize: Boolean): DataSource = new DataSource(pathName, normalize, true)
   def apply(fileName: String, pathName: String, normalize: Boolean): DataSource = new DataSource(fileName, normalize, true)
 }
