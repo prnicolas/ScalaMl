237624e80d46489127855943719ce654ceb28620
prnicolas <pnicolas57@yahoo.com>
2014-06-26 09:45:52 -0700

    Add evaluation classes for regression

--- src/main/scala/org/scalaml/app/chap6/Chap6.scala
+++ src/main/scala/org/scalaml/app/chap6/Chap6.scala
@@ -6,80 +6,40 @@
  */
 package org.scalaml.app.chap6
 
-import org.scalaml.util.Matrix
-import org.scalaml.supervised.hmm.{HMM, HMMForm, HMMLambda}
-import org.scalaml.supervised.crf.{CrfConfig,  CrfSeqDelimiter, CrfLinearChain}
-import java.io.IOException
-
-
-object CRFTest {
-  def test: Unit = {
-    Console.println("CRFTest.test")
-    
-	val l2Penalty = 0.5
-	val nLabels = 9
-	val config = CrfConfig(0.7, 100, l2Penalty, 0.01)
-	val delimiters = CrfSeqDelimiter(",\t/ -():.;'?#`&_", "//", "\n")
-	    
-	try {
-	   val crf = new CrfLinearChain(nLabels, config, delimiters, "resources/data/chap6/rating")
-	   val lambdas = crf.lambdas
-	   if( lambdas == null)
-		   println("Count not train the CRF model")
-	   else
-		   lambdas.foreach( println )
-	 }
-     catch {
-		case e: IllegalArgumentException => Console.println(e.toString)
-		case e: IOException => Console.println(e.toString)
-	 }
-  }
-}
-
-
-object HMMTest {
-  def test: Unit = {
-  	 Console.println("HMMTest.test")
-  	 import HMM._, HMMForm._
-   
-     val pi = Array[Double](0.6, 0.4)
-     val A = Matrix[Double](2, 2)
-     A += (0, 0, 0.7)
-     A += (0, 1, 0.3)
-     A += (1, 0, 0.4)
-     A += (1, 1, 0.6)
-   
-     val B = Matrix[Double](2, 3)
-     B += (0, 0, 0.1)
-     B += (0, 1, 0.4)
-     B += (0, 2, 0.5)
-     B += (1, 0, 0.7)
-     B += (1, 1, 0.2)
-     B += (1, 2, 0.1)
-   
-    for( i <- 0 until 2) {
-  	   for (k <- 0 until 3) 
-  		 println("(" + i + "," + k + ") =" + B(i,k) )
-    }  
-   
-    val lambda = HMMLambda(4, pi, A, B)
-    val hmm = HMM[Array[Int]](lambda, EVALUATION)
-  }
-}
-
-
 
 
 object Chap6 extends App {
-   if( args == null)
-  	 Console.println("Program arguments missing: Chap6 [HNN | CRF]" )
-   else {
-  	 args(1) match {
-  		 case "HMM" => HMMTest.test
-  		 case "CRF" => CRFTest.test
-  		 case _ => Console.println("Incorrect program argument " + args(1))
-  	 }
-   }
+	private def runAll = {
+		SingleLinearRegressionEval.run()
+		MultiLinearRegressionEval.run(Array[String]("trend"))
+		MultiLinearRegressionEval.run(Array[String]("filter"))
+		RidgeRegressionEval.run()
+		LogisticRegressionEval.run()
+	}
+	
+	final val cmdDescriptor: String = {
+		new StringBuilder("Command line: Chap 6 arg\n")
+		   .append(" singlelinear: Evaluation single linear regression\n")
+		   .append(" multilinear:  Evaluation ordinary Least Squares regression\n")
+		   .append(" ridge: Evaluation Ridge regression\n")
+		   .append(" logistic: Evaluation Logistic regression\n")
+		   .append(" all: All evaluation").toString
+	}
+	
+	val argument = if( args == null && args.length == 0) "?" else args(0)
+	argument match {
+		case "?" => println(cmdDescriptor)
+		case "singlelinear" =>  SingleLinearRegressionEval.run()
+		case "multilinear" => {
+			MultiLinearRegressionEval.run(Array[String]("trend"))
+			MultiLinearRegressionEval.run(Array[String]("filter"))
+		}
+		case "ridge" => RidgeRegressionEval.run()
+		case "logistic" => LogisticRegressionEval.run()
+		case "all" => runAll
+		case _ =>  println(cmdDescriptor)
+	}
 }
 
-// --------------------------------  EOF -------------------------------
+
+// ----------------------------  EOF ----------------------------------
