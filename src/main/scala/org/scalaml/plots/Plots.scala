6c34107f7e8de3ae13128d442bd7f812239183e1
prnicolas <pnicolas57@yahoo.com>
2014-07-15 00:36:13 -0700

    Add implementation of multiple time series for Scatter plots

--- src/main/scala/org/scalaml/plots/Plots.scala
+++ src/main/scala/org/scalaml/plots/Plots.scala
@@ -16,13 +16,17 @@ import org.jfree.chart.ChartFrame
 import org.jfree.util.ShapeUtilities
 import org.jfree.chart.renderer.xy.{XYDotRenderer, XYLineAndShapeRenderer}
 import Plot._
-
-
 import org.jfree.data.category.{DefaultCategoryDataset, CategoryDataset}
 import org.jfree.chart.renderer.category.LineAndShapeRenderer
 import org.jfree.data.statistics.DefaultMultiValueCategoryDataset
 import java.util.List
 import org.scalaml.core.Types
+import Types.ScalaMl._
+import org.jfree.data.xy.XYDataset
+import java.awt.geom.Ellipse2D
+import org.jfree.chart.renderer.xy.XYShapeRenderer
+import org.jfree.chart.axis.ValueAxis
+import org.jfree.chart.axis.NumberAxis
 
 
 
@@ -30,12 +34,6 @@ object ChartType extends Enumeration {
 	type ChartType = Value
 	val LINE, TIME_SERIES, SCATTER, BAR = Value
 }
-
-
-		/**
-		 * <p>Generic base class for plots using the JFreeChart library.
-		 */
-import Types.ScalaMl._
 abstract class Plot(val config: PlotInfo, val theme: PlotTheme) {
    require(config != null, "Cannot create a plot with undefined configuration")
    require(theme != null, "Cannot create a plot with undefined theme")
@@ -139,23 +137,104 @@ class ScatterPlot(val _config: PlotInfo, val _theme: PlotTheme) extends Plot(_co
       require( w > 60 && w < 1260, "Width " + w + " is out of range")
       require( h > 60 && h < 1260, "height " + h + " is out of range")
           
-	  val series = new XYSeries(config._1)
-	  xy.foreach( x => series.add( x._1,x._2))
+	  val series =  xy.foldLeft(new XYSeries(config._1))((s, xy) => {s.add(xy._1, xy._2); s})
 	  val seriesCollection = new XYSeriesCollection
 	  seriesCollection.addSeries(series)
-	    
+	  draw(seriesCollection, w, h)
+   }
+   
+   def display(xy1: XYTSeries, xy2: XYTSeries, w: Int, h : Int): Unit  = {
+  	  require( xy1 != null && xy1.size > 0 && xy2 != null && xy2.size >0, "Cannot display an undefined series")
+      require( w > 60 && w < 1260, "Width " + w + " is out of range")
+      require( h > 60 && h < 1260, "height " + h + " is out of range")
+          
+      val seriesCollection1 = new XYSeriesCollection
+       val seriesCollection2 = new XYSeriesCollection
+	  val series1 = xy1.foldLeft(new XYSeries(config._1))((s, xy) => {s.add(xy._1, xy._2); s})
+	  val series2 = xy2.foldLeft(new XYSeries(config._1))((s, xy) => {s.add(xy._1, xy._2); s})
+	  seriesCollection1.addSeries(series1)
+	  seriesCollection2.addSeries(series2)
+	  
+	  val chart = ChartFactory.createScatterPlot(config._2, config._2, config._3, seriesCollection1, PlotOrientation.VERTICAL, true, false, false)
+      val plot = chart.getPlot.asInstanceOf[XYPlot]
+      val renderer1 = new XYDotRenderer
+      plot.setRenderer(0, renderer1)
+      plot.setDomainAxis(0, new NumberAxis("x"))
+      renderer1.setSeriesPaint(0, theme.color(0))
+      renderer1.setDotHeight(4)
+      renderer1.setDotWidth(4)
+      
+      plot.setDataset(1, seriesCollection2)
+      val renderer2 = new XYShapeRenderer
+      plot.setRenderer(1, renderer2)
+      renderer2.setSeriesShape(0, ShapeUtilities.createDiamond(3.0F))
+      renderer2.setSeriesPaint(0, theme.color(1))
+   
+   
+	  plot.setBackgroundPaint(theme.paint(w, h))
+	  createFrame("First", chart)
+   }
+   
+   def display(xs: scala.List[XYTSeries], lbls: scala.List[String], w: Int, h : Int): Unit  = {
+  	  require(xs != null && xs.size > 0, "Cannot display an undefined series")
+      require( w > 60 && w < 1260, "Width " + w + " is out of range")
+      require( h > 60 && h < 1260, "height " + h + " is out of range")
+          
+      val seriesCollectionsList = xs.zipWithIndex.foldLeft(scala.List[XYSeriesCollection]())((xs, xy) => { 
+      	  val seriesCollection = new XYSeriesCollection
+      	  val series = xy._1.foldLeft(new XYSeries(lbls(xy._2)))((s, t) => {s.add(t._1, t._2); s})
+      	  seriesCollection.addSeries(series)
+      	  seriesCollection :: xs
+      })
+      /*
+      val seriesCollection1 = new XYSeriesCollection
+      val seriesCollection2 = new XYSeriesCollection
+	  val series1 = x.foldLeft(new XYSeries(config._1))((s, xy) => {s.add(xy._1, xy._2); s})
+	  val series2 = y.foldLeft(new XYSeries(config._1))((s, xy) => {s.add(xy._1, xy._2); s})
+	  seriesCollection1.addSeries(series1)
+	  seriesCollection2.addSeries(series2)
+	  * 
+	  */
+	  
+	  val chart = ChartFactory.createScatterPlot(config._2, config._2, config._3, seriesCollectionsList.last, PlotOrientation.VERTICAL, true, false, false)
+      val plot = chart.getPlot.asInstanceOf[XYPlot]
+  	  
+      val renderer1 = new XYDotRenderer
+      plot.setRenderer(0, renderer1)
+      plot.setDomainAxis(0, new NumberAxis("Trading sessions"))
+      renderer1.setSeriesPaint(0, theme.color(0))
+      renderer1.setDotHeight(4)
+      renderer1.setDotWidth(4)
+   
+      
+      val shapes = Array[Shape](ShapeUtilities.createDiamond(3.0F), ShapeUtilities.createRegularCross(2.0F, 2.0F))
+      seriesCollectionsList.take(seriesCollectionsList.size-1).zipWithIndex.foreach( sColi =>{ 
+      	  plot.setDataset(sColi._2+1, sColi._1)
+          val renderer = new XYShapeRenderer
+          plot.setRenderer(sColi._2+1, renderer)
+          renderer.setSeriesShape(0, shapes(sColi._2%shapes.size))
+          renderer.setSeriesPaint(0, theme.color(sColi._2+1))
+      })
+   
+	  plot.setBackgroundPaint(theme.paint(w, h))
+	  createFrame("First", chart)
+   }
+   
+   	    
+    private def draw(seriesCollection: XYSeriesCollection, w: Int, h : Int) {
       val chart = ChartFactory.createScatterPlot(config._2, config._2, config._3, seriesCollection, PlotOrientation.VERTICAL, true, false, false)
       val plot = chart.getPlot.asInstanceOf[XYPlot]
       val renderer = new XYDotRenderer
       plot.setRenderer(renderer)
-      renderer.setSeriesShape(0, ShapeUtilities.createDiamond(6.0F))
-      renderer.setSeriesPaint(0, theme.color(0))
-      renderer.setDotHeight(6)
-      renderer.setDotWidth(6)
+      renderer.setSeriesShape(0, ShapeUtilities.createDiamond(8.0F))
+      renderer.setSeriesPaint(0, theme.color(3))
+      renderer.setDotHeight(10)
+      renderer.setDotWidth(10)
    
 	  plot.setBackgroundPaint(theme.paint(w, h))
 	  createFrame("First", chart)
     }
+   
 }
 
 
