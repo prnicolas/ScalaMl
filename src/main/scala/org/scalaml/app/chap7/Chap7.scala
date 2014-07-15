59178bf5fd532763e9b2b5d273c96ee6491af013
prnicolas <pnicolas57@yahoo.com>
2014-07-15 00:25:13 -0700

    streamlined evalation code

--- src/main/scala/org/scalaml/app/chap7/Chap7.scala
+++ src/main/scala/org/scalaml/app/chap7/Chap7.scala
@@ -12,63 +12,14 @@ import org.scalaml.supervised.crf.{CrfConfig,  CrfSeqDelimiter, CrfLinearChain}
 import java.io.IOException
 
 
-object CRFEval {
-  def run: Unit = {
-    Console.println("Evaluation of Conditional Random Fields")
-    
-	val lambda = 0.5
-	val nLabels = 9
-	val config = CrfConfig(0.7, 100, lambda, 0.01)
-	val delimiters = CrfSeqDelimiter(",\t/ -():.;'?#`&_", "//", "\n")
-	    
-	try {
-	   val crf = new CrfLinearChain(nLabels, config, delimiters, "resources/data/chap6/rating")
-	   crf.weights match {
-	  	 case Some(weights) => weights.foreach( println )
-	  	 case None =>  println("Count not train the CRF model")
-	   }
-	 }
-     catch {
-		case e: IllegalArgumentException => Console.println(e.toString)
-		case e: IOException => Console.println(e.toString)
-	 }
-  }
-}
 
-
-object HMMEval {
-  def run: Unit = {
-  	 Console.println("Evaluation of Hidden Markov Models")
-  	 import HMM._, HMMForm._
-   
-     val pi = Array[Double](0.6, 0.4)
-     val A = Matrix[Double](2, 2)
-     A += (0, 0, 0.7)
-     A += (0, 1, 0.3)
-     A += (1, 0, 0.4)
-     A += (1, 1, 0.6)
-   
-     val B = Matrix[Double](2, 3)
-     B += (0, 0, 0.1)
-     B += (0, 1, 0.4)
-     B += (0, 2, 0.5)
-     B += (1, 0, 0.7)
-     B += (1, 1, 0.2)
-     B += (1, 2, 0.1)
-   
-    for( i <- 0 until 2) {
-  	   for (k <- 0 until 3) 
-  		 println("(" + i + "," + k + ") =" + B(i,k) )
-    }  
-   
-    val lambda = HMMLambda(4, pi, A, B)
-    val hmm = HMM[Array[Int]](lambda, EVALUATION)
-  }
-}
-
-
-
-
+	/**
+	 * Singleton to execute the test cases presented in Chapte7 7
+	 * 
+	 * @author Patrick Nicolas
+	 * @date March 25, 2014
+	 * @project Scala for Machine Learning
+	 */
 object Chap7 extends App {
    private def runAll = {
 	 HMMEval.run
@@ -82,7 +33,7 @@ object Chap7 extends App {
 		  .append(" all: All test cases").toString
   }
   
-  val argument = if( args == null && args.length == 0) "?" else args(0)
+  val argument = if( args == null || args.length == 0) "?" else args(0)
 
   argument match {
   	 case "?" => println(cmdDescriptor)
