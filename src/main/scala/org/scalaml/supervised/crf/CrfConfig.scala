299413e1e911d4080478808fb1e315a92ea0207a
prnicolas <pnicolas57@yahoo.com>
2014-07-30 23:28:08 -0700

    Update to the CRF implementation

---
+++ src/main/scala/org/scalaml/supervised/crf/CrfConfig.scala
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2013, 2014  by Patrick Nicolas - Scala for Machine Learning - All rights reserved
+ *
+ * The source code in this file is provided by the author for the only purpose of illustrating the 
+ * concepts and algorithms presented in Scala for Machine Learning.
+ * 
+ * This code uses the iitb CRF library 
+ * Copyright (c) <2004> <Sunita Sarawagi Indian Institute of Technology Bombay> All rights reserved.
+ */
+package org.scalaml.supervised.crf
+
+import iitb.CRF.{CRF, CrfParams, DataSequence, DataIter, FeatureGenerator}
+import iitb.Model.{FeatureGenImpl, CompleteModel}
+import org.scalaml.core.XTSeries
+import org.scalaml.workflow.data.DataSource
+import org.scalaml.workflow.PipeOperator
+import org.scalaml.supervised.Supervised
+import java.io.IOException
+import org.scalaml.core.Types.ScalaMl._
+
+	/**
+	 * <p>Class that defines the basic configuration of the CRF algorithm. The class generates a textual
+	 * description of the configuration of CRF used by iitb library </p>
+	 * @param initLambda  initial values for the CRF weights/factors (lambdas)
+	 * @param maxIters  Maximum number of iterations to be used for the training of CRF
+	 * @param lambda L2-regularization penalty function 1/square(sigma) used in the log likelihood log p(Y|X)
+	 * @param eps Convergence criteria used on the log likelihood  delta( log p(Y|X)to exit from the training iteration
+	 * @param debug Optional debugging flag 
+	 * 
+	 * @author Patrick Nicolas
+	 * @date April 3, 2014
+	 */
+import CrfConfig._
+class CrfConfig(val initW: Double, val maxIters: Int, val lambda: Double, val eps:Double, val debug: Int) {
+		// textual description of the CRF configuration
+	val params = new StringBuilder().append("initValue ").append(String.valueOf(initW))
+		           .append(" maxIters ").append(String.valueOf(maxIters)).append(" lambda ")
+		              .append(String.valueOf(lambda)).append( " scale ")
+		                 .append(" true" ).append(" epsForConvergence ").append(String.valueOf(eps) )
+		                   .append(" debugLvl ").append(debug).toString
+
+	private def validate(initW: Double, maxIters: Int, lambda: Double,  eps: Double): Unit = {
+		require( initW >= INIT_WEIGHTS_LIMITS._1 && initW <= INIT_WEIGHTS_LIMITS._2, 
+				        "Initialization of the CRF weights " + initW + " is out of range")
+		require( maxIters >= MAX_ITERS_LIMITS._1 && maxIters <= MAX_ITERS_LIMITS._2, 
+				         "Maximum number of iterations for CRF training " + maxIters + " is out of range")
+		require( lambda >= LAMBDA_LIMITS._1 && lambda <= LAMBDA_LIMITS._2, 
+				         "The factor for the L2 penalty for CRF" + lambda + " is out of range")
+		require( eps > EPS_LIMITS._1 && eps<= EPS_LIMITS._2, 
+				          "The convergence criteria for the CRF training " + eps + " is out of range")
+    }
+}
+
+
+		/**
+		 * <p>Companion object for the configuration of the conditional random field. The
+		 * singleton is used to define constructors and boundaries for the class parameters..</p>
+		 */
+object CrfConfig {
+	final val INIT_WEIGHTS_LIMITS = (1.0, 2.5)
+	final val MAX_ITERS_LIMITS = (10, 250)
+	final val LAMBDA_LIMITS = (1e-15, 1.5)
+	final val EPS_LIMITS = (1e-5, 0.2)
+	
+	def apply(initW: Double, maxIters: Int, lambda: Double, eps:Double, debug: Int): CrfConfig = 
+		            new CrfConfig(initW, maxIters, lambda, eps, debug)
+	
+    def apply(initW: Double, maxIters: Int, lambda: Double, eps:Double): CrfConfig = 
+		            new CrfConfig(initW, maxIters, lambda, eps, 0)
+}
+
+
+
+// ---------------------------- EOF ------------------------------------------------------
---
+++ src/main/scala/org/scalaml/supervised/crf/CrfLinearChain.scala
@@ -0,0 +1,101 @@
+/**
+ * Copyright 2013, 2014  by Patrick Nicolas - Scala for Machine Learning - All rights reserved
+ *
+ * The source code in this file is provided by the author for the only purpose of illustrating the 
+ * concepts and algorithms presented in Scala for Machine Learning.
+ * 
+ * This code uses the iitb CRF library 
+ * Copyright (c) <2004> <Sunita Sarawagi Indian Institute of Technology Bombay> All rights reserved.
+ */
+package org.scalaml.supervised.crf
+
+import iitb.CRF.{CRF, CrfParams, DataSequence, DataIter, FeatureGenerator}
+import iitb.Model.{FeatureGenImpl, CompleteModel}
+import org.scalaml.core.XTSeries
+import org.scalaml.workflow.data.DataSource
+import org.scalaml.workflow.PipeOperator
+import org.scalaml.supervised.Supervised
+import java.io.IOException
+import org.scalaml.core.Types.ScalaMl._
+
+
+import CrfConfig._
+import CrfLinearChain._
+
+
+	/**
+	 * <p>Generic class for the linear chained CRF for tagging words, N-Grams or regular expression. The class
+	 * define a Feature generator class that inherits the default implementation FeatureGenImpl of iitb features generator.
+	 * The class assumes the training sequences are loaded from file with *.raw and *.tagged extensions.
+	 * @param nLabels Number of labels (or tags) used in tagging training sequences of observations.
+	 * @param config minimalist set of configuration parameters used in the CRF
+	 * @param delims delimiters used in extracting labels and data from the training files
+	 * @param taggedObs identifier for the training data set. The training set of sequences is defined by the raw observations
+	 * and its associated tagged file    taggedObs.raw and taggedObs.tagged files.
+	 * @exception IllegalArgumentException if nLabels, config, delims and taggedObs are either undefined or out of range.
+	 * @see org.scalaml.workflow.PipeOperator
+	 * 
+	 * @author Patrick Nicolas
+	 * @data April 3, 2014
+	 */
+class CrfLinearChain(val nLabels: Int, val config: CrfConfig, val delims: CrfSeqDelimiter, val taggedObs: String) 
+                                               extends PipeOperator[String, Double] with Supervised[String] {
+	
+  validate(nLabels, config, delims, taggedObs)
+  
+  class TaggingGenerator(nLabels: Int) extends FeatureGenImpl(new CompleteModel(nLabels) , nLabels, true)
+	
+  private[this] val features = new TaggingGenerator(nLabels)
+  private[this] lazy val crf = new CRF(nLabels, features, config.params)
+  
+  private val model: Option[CrfModel] = {
+  	 val seqIter = CrfSeqIter(nLabels: Int, taggedObs, delims)
+  	 try {
+	  	 features.train(seqIter)
+	  	 Some(crf.train(seqIter))
+  	 }
+  	 catch {
+  		 case e: IOException => Console.println(e.toString); None
+  		 case e: Exception  =>Console.println(e.toString); None
+  	 }
+  }
+  
+  		/**
+  		 * Main training method for the CRF
+  		 */
+
+  override def |> (obs: String): Option[Double] = model match {
+  	case Some(w) => {
+	  	 val dataSeq =  new CrfRecommendation(nLabels, obs, delims.dObs)
+	  	 Some(crf.apply(dataSeq))
+  	}
+  	case None => None
+  }
+  
+  def weights: Option[DblVector] = model
+  
+  override def validate(output: XTSeries[(Array[String], Int)], index: Int): Double = -1.0
+  
+   private def validate(nLabels: Int, config: CrfConfig, delims: CrfSeqDelimiter, taggedObs: String): Unit = {
+	  require(nLabels > NUM_LABELS_LIMITS._1 && nLabels < NUM_LABELS_LIMITS._2, "Number of labels for generating tags for CRF " + nLabels + " is out of range")
+	  require(config != null, "Configuration of the linear Chain CRF is undefined")
+	  require(delims != null, "delimiters used in the CRF training files are undefined")
+	  require(taggedObs != null, "Tagged observations used in the CRF training files are undefined")
+  }
+}
+
+
+
+	/**
+	 * Companion object for the Linear chained Conditional random field. The singleton is used
+	 * to define the constructors.
+	 */
+object CrfLinearChain {
+	final val NUM_LABELS_LIMITS = (1, 200)
+	type CrfModel = DblVector
+	def apply(nLabels: Int, config: CrfConfig, delims: CrfSeqDelimiter, taggedObs: String): CrfLinearChain = 
+		           new CrfLinearChain(nLabels, config, delims, taggedObs)
+}
+
+
+// ---------------------------- EOF ------------------------------------------------------
