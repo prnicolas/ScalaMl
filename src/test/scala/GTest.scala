b4d15df8720b2278c6ea26201a66ba7d0a4e71dd
prnicolas <pnicolas57@yahoo.com>
2014-08-20 22:30:18 -0700

    updated test

--- src/test/scala/GTest.scala
+++ src/test/scala/GTest.scala
@@ -1,10 +1,15 @@
 
 
 
-object GTest extends App {
-   val series = Array[Double](1.9, 1., 4.0, 5.9, 11)
+import org.apache.log4j.Logger
+
+object GTestApp extends App {
+   val series = Array[Double](1.9, 1.0, 4.0, 5.9, 11)
    val r: Range = 0 until 5
    r filter( _ != 3) foreach( println )
+   
+   val logger = Logger.getLogger("GetTestApp")
+   logger.info("Hello")
    
 }
 
