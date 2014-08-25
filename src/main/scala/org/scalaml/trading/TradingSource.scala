e330d7ff3eaa194615b427ef67686a322fc6ed27
prnicolas <pnicolas57@yahoo.com>
2014-08-25 01:08:19 -0700

    Replace all files for financials sources

--- src/main/scala/org/scalaml/trading/TradingSource.scala
+++ src/main/scala/org/scalaml/trading/TradingSource.scala
@@ -6,6 +6,8 @@
  */
 package org.scalaml.trading
 
+import scala.util.{Try, Success, Failure}
+import org.scalaml.util.Display
 
 
 	/**
@@ -31,13 +33,11 @@ object YahooFinancials extends Enumeration {
    def +(value1: Value, value2: Value): Array[String] => Double = (s: Array[String]) => s(value1.id).toDouble + s(value2.id).toDouble
    def *(value1: Value, value2: Value): Array[String] => Double = (s: Array[String]) => s(value1.id).toDouble * s(value2.id).toDouble
    def vol: Array[String] => Double = (s: Array[String]) => {
-  	  try {
-  	     (s(HIGH.id).toDouble/s(LOW.id).toDouble -1.0)*s(VOLUME.id).toDouble
-   	  }
-      catch {
-         case e: RuntimeException => -1
-         case e: NumberFormatException => -2
-      } 
+  	  Try ( (s(HIGH.id).toDouble/s(LOW.id).toDouble -1.0)*s(VOLUME.id).toDouble  )
+  	   match {
+  	  	case Success(res) => res
+  	  	case Failure(e) => Display.error("YahooFinancials.vol ", e); -1
+  	  }
    }
 }
 
@@ -51,6 +51,44 @@ object GoogleFinancials extends Enumeration {
    val volatility = ((s:Array[String]) => s(HIGH.id).toDouble - s(LOW.id).toDouble)
    val volatilityVol = ((s:Array[String]) => ((s(HIGH.id).toDouble - s(LOW.id).toDouble), s(VOLUME.id).toDouble))
 }
+
+
+
+		/**
+		 * <p>Enumeration used extract data for evaluating risk on dividend pay-out. 
+		 * Object methods are implemented to load the appropriate field and perform the type conversion</p>
+		 * 
+		 * @author Patrick Nicolas
+		 * @since May 3, 2014
+		 * @note Scala for Machine Learning
+		 */
+object Fundamentals extends Enumeration {
+   type Fundamentals = Value
+   val TICKER, 
+       START_PRICE, 
+       END_PRICE, 
+       RELATIVE_PRICE_CHANGE, 
+       DEBT_TO_EQUITY, 
+       DIVIDEND_COVERAGE, 
+       OPERATING_MARGIN, 
+       SHORT_INTEREST, 
+       CASH_PER_SHARE,
+       CASH_PER_SHARE_TO_PRICE, 
+       EPS_TEND, 
+       DIVIDEND_YIELD,
+       DIVIDEND_TREND = Value
+       
+   val ticker = ((s:Array[String]) => s(TICKER.id))
+   val relPriceChange =  ((s:Array[String]) => s(RELATIVE_PRICE_CHANGE.id).toDouble)
+   val debtToEquity = ((s:Array[String]) => s(DEBT_TO_EQUITY.id).toDouble)
+   val dividendCoverage = ((s:Array[String]) => s(DIVIDEND_COVERAGE.id).toDouble)
+   val shortInterest = ((s:Array[String]) => s(SHORT_INTEREST.id).toDouble)
+   val cashPerShareToPrice = ((s:Array[String]) => s(CASH_PER_SHARE_TO_PRICE.id).toDouble)
+   val epsTrend = ((s:Array[String]) => s(EPS_TEND.id).toDouble)
+   val dividendYield = ((s:Array[String]) => s(DIVIDEND_YIELD.id).toDouble)
+   val dividendTrend = ((s:Array[String]) => s(DIVIDEND_TREND.id).toDouble)
+}
+
 
 
 
