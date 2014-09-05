dcb73bb5a259a73f5cfad51cdaf47a6a26a5065d
prnicolas <pnicolas57@yahoo.com>
2014-09-05 02:16:06 -0700

    Add constructor for Matrix class

--- src/main/scala/org/scalaml/util/Matrix.scala
+++ src/main/scala/org/scalaml/util/Matrix.scala
@@ -14,42 +14,52 @@ import org.scalaml.core.Types.ScalaMl._
 
 
 		/**
+		 * <p>Class that contains basic Matrix manipulation</p>
 		 *  @author Patrick Nicolas
 		 *  @since Feb 23, 2014
-		 *  @note Book
+		 *  @note Scala for Machine Learning
 		 */
 
 class Matrix[@specialized(Double, Int) T: ClassTag](val nRows: Int, val nCols: Int, val data: Array[T])(implicit f: T => Double) {
-
-   
- //  def this(xy: Array[Array[T]])(implicit f: T => Double) = this(xy.size, xy(0).size, xy.flatten)
- //  def this(nCols: Int, nRows: Int)(implicit f: T => Double) = this(nCols, nRows, new Array[T](nCols*nRows))
   
-   def apply(i: Int, j: Int): T = data(i*nCols+j)
+   final def apply(i: Int, j: Int): T = data(i*nCols+j)
    def += (i: Int, j : Int, t: T): Unit = data(i*nCols +j) = t
    
-   def diff(that: Matrix[T], error: (T, T) => Double)(implicit f: T => Double): Double = {
+   final def diff(that: Matrix[T], error: (T, T) => Double)(implicit f: T => Double): Double = {
   	  require( nCols == that.nCols && nRows == that.nRows, "cannot compare Matrices of different sizes")
   	  
   	  data.zip(that.data).foldLeft(0.0)((s, xy) => s + error(xy._1, xy._2))
    }
    
-   def cols(i: Int): Array[T] = { val idx = i*nRows; data.slice(idx, idx + nRows) }
+   final def cols(i: Int): Array[T] = { 
+  	 val idx = i*nRows
+  	 data.slice(idx, idx + nRows) 
+   }
    
    def /= (i: Int, t: T)(implicit g: Double => T): Unit =  {
+  	  val iRow = i*nRows
+  	  Range(0, nRows).foreach(k => data(iRow + k) /= t)
+  	  /*
   	  var k = 0
   	  while(k < nRows ) {
-  	     data(i*nRows +k) /= t
+  	     data(iRow +k) /= t
   	     k += 1
   	  }
+  	  * 
+  	  */
    }
    
    def += (i: Int, t: T): Unit = {
+  	  val iRow = i*nRows
+  	  Range(0, nRows).foreach(k => data(iRow + k) =t)
+  	  /*
   	  var k = 0
   	  while(k < nRows ) {
   	     data(i*nRows +k) = t
   	     k += 1
   	  }
+  	  * 
+  	  */
    }
    
    
@@ -73,9 +83,7 @@ class Matrix[@specialized(Double, Int) T: ClassTag](val nRows: Int, val nCols: I
 
 
 object Matrix {
-   import org.apache.commons.math3.linear.{Array2DRowRealMatrix, RealMatrix }
-   implicit def matrix2RealMatrix(m: Matrix[Double]): RealMatrix = new Array2DRowRealMatrix(m.data)
-   
+   def apply[T: ClassTag](nRows: Int)(implicit f: T => Double): Matrix[T] = apply(nRows, nRows)
    def apply[T: ClassTag](nRows: Int, nCols: Int, data: Array[T])(implicit f: T => Double): Matrix[T] = new Matrix(nRows, nCols, data)
    def apply[T: ClassTag](nRows: Int, nCols: Int)(implicit f: T => Double): Matrix[T] = new Matrix(nRows, nCols, new Array[T](nCols*nRows))
    def apply[T: ClassTag](xy: Array[Array[T]])(implicit f: T => Double): Matrix[T] = new Matrix(xy.size, xy(0).size, xy.flatten)
