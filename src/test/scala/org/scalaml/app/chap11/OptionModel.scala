32f61f988dc29f770dddc77e0f3f0aade22fbd7f
prnicolas <pnicolas57@yahoo.com>
2014-10-29 21:34:55 -0700

    Update test cases for Chapter 11

---
+++ src/test/scala/org/scalaml/app/chap11/OptionModel.scala
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2013, 2014  by Patrick Nicolas - Scala for Machine Learning - All rights reserved
+ *
+ * The source code in this file is provided by the author for the sole purpose of illustrating the 
+ * concepts and algorithms presented in "Scala for Machine Learning" ISBN: 978-1-783355-874-2 Packt Publishing.
+ * Unless required by applicable law or agreed to in writing, software is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * Version 0.94
+ */
+package org.scalaml.app.chap11
+
+import org.scalaml.reinforcement.qlearning._
+import org.scalaml.plots.{ScatterPlot, LinePlot, LightPlotTheme}
+import org.scalaml.workflow.data.DataSource
+import org.scalaml.core.XTSeries
+import org.scalaml.trading.YahooFinancials
+import YahooFinancials._
+import org.scalaml.core.types.ScalaMl.DblVector
+import org.apache.log4j.Logger
+import org.scalaml.util.Display
+import scala.collection.mutable.ArrayBuffer
+import org.scalaml.util.{Counter, NumericAccumulator}
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.ListBuffer
+
+class OptionModel(symbol: String, strikePrice: Double, src: DataSource, minExpT: Int, nSteps: Int) {
+   import YahooFinancials._
+   val price = src |> adjClose
+     
+   val futurePrice = price.drop(2)
+   val propsList: List[OptionProperty] = {
+       val rVolatility = XTSeries.normalize((src |> relVolatility).toArray).get
+       val volByVol = XTSeries.normalize((src |> volatilityByVol).toArray).get
+       val relPriceToStrike = XTSeries.normalize(price.map(p => 1.0 - strikePrice/p)).get
+       
+  	   rVolatility.zipWithIndex
+  	              .foldLeft(List[OptionProperty]())((xs, e) => {
+  	      val normDecay = (e._2+minExpT).toDouble/(price.size+minExpT)
+  	      new OptionProperty(normDecay, e._1, volByVol(e._2), relPriceToStrike(e._2)) :: xs
+  	   }).drop(2).reverse
+   }
+   	
+   def approximate(y: DblVector): Map[Array[Int], Double] = {
+  	  val mapper = new HashMap[Int, Array[Int]]
+  	    
+  	  val acc = new NumericAccumulator
+  	  propsList.map( _.toArray)
+  	             .map( toArrayInt( _ ))
+  	             .map(ar => { val enc = encode(ar); mapper.put(enc, ar); enc})
+  	             .zip(y)
+  	             .foldLeft(acc)((acc, t) => {acc += (t._1, t._2); acc })
+  	  
+  	  acc.map(kv => (kv._1, kv._2._2/kv._2._1))
+  	     .map(kv => (mapper(kv._1), kv._2)).toMap
+   }
+   
+  
+   private def encode(arr: Array[Int]): Int = 
+       arr.foldLeft((1, 0))((s, n) => {
+    	  val np = s._1*n
+    	  (s._1*nSteps, s._2 + np)
+       })._2
+
+    	  
+   private def toArrayInt(feature: DblVector): Array[Int] = feature.map(x => (nSteps*x).floor.toInt)
+}
+
+
+class OptionProperty(timeToExp: Double, relVolatility: Double, volatilityByVol: Double, relPriceToStrike: Double) {
+   val toArray = Array[Double](timeToExp, relVolatility, volatilityByVol, relPriceToStrike)
+}
+
+
+// ------------------------------------  EOF ----------------------------------
---
+++ src/test/scala/org/scalaml/app/chap11/QLearningEval2.scala
@@ -0,0 +1,88 @@
+/**
+ * Copyright 2013, 2014  by Patrick Nicolas - Scala for Machine Learning - All rights reserved
+ *
+ * The source code in this file is provided by the author for the sole purpose of illustrating the 
+ * concepts and algorithms presented in "Scala for Machine Learning" ISBN: 978-1-783355-874-2 Packt Publishing.
+ * Unless required by applicable law or agreed to in writing, software is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * Version 0.94
+ */
+package org.scalaml.app.chap11
+
+import org.scalaml.reinforcement.qlearning.{QLearning, QLState, QLConfig}
+import org.scalaml.plots.{ScatterPlot, LinePlot, LightPlotTheme}
+import org.scalaml.workflow.data.DataSource
+import org.scalaml.core.XTSeries
+import org.scalaml.trading.YahooFinancials
+import YahooFinancials._
+import org.scalaml.core.types.ScalaMl.DblVector
+import org.apache.log4j.Logger
+import org.scalaml.util.Display
+
+
+
+	 
+object QLearningEval2 {
+    			/*
+    			 * Initial data set extracted from stock CSV files
+    			 */
+    final val alpha = 0.6
+	final val gamma = 0.5
+	final val maxIters = 150
+			
+	final val path = "resources/data/chap11/"
+	final val etfs = Array[String]("TLT", "ICF", "SPY", "VWO", "USO", "IWC")
+	
+	private val logger = Logger.getLogger("QLearningEval")
+
+	/*
+	final val states = Array[QLState[Double]](
+	   QLState[Double](0, Array[Double](0.2, 0.2, 0.2, 0.0, 0.2, 0.0)),
+	   QLState[Double](1, Array[Double](0.1, 0.1, 0.1, 0.1, 0.3, 0.3)),
+	   QLState[Double](2, Array[Double](0.3, 0.3, 0.1, 0.1, 0.1, 0.1)),
+	   QLState[Double](3, Array[Double](0.1, 0.2, 0.2, 0.2, 0.2, 0.1)),
+	   QLState[Double](4, Array[Double](0.5, 0.1, 0.1, 0.1, 0.1, 0.1)),
+	   QLState[Double](5, Array[Double](0.2, 0.3, 0.0, 0.2, 0.2, 0.1)),
+	   QLState[Double](6, Array[Double](0.0, 0.0, 0.3, 0.2, 0.2, 0.2)),
+	   QLState[Double](7, Array[Double](0.3, 0.0, 0.0, 0.2, 0.1, 0.4)),
+	   QLState[Double](8, Array[Double](0.4, 0.1, 0.1, 0.0, 0.2, 0.2))
+	)
+	* 
+	*/
+	
+	
+    def run: Unit = {	
+    	println("Evaluation Q-Learning algorithm")
+    		// stateuration of QLearning algorithm
+    	/*
+	  	val qConfig = new QLConfig(alpha, gamma, episodeLength; numEpisodes, minQuality)
+	  	
+	  		// extract the input values for each symbol (stock time series)
+	  	val values = etfs.map( etf =>
+	  		DataSource(path + etf + ".csv", false, true, 1) |> YahooFinancials.adjClose )
+	  		
+	  		// If all values have been retrieved
+	    values find( _ != None) match {
+		   case Some(input) => {
+		  	  val labels = List[(DblVector, QLState[Double])]()
+		  	  
+		  			  // start collecting the training labels
+		  	  Range(40, 240).foreach( n => {
+			     val goal = states.zipWithIndex
+			                       .maxBy(p => p._1.distribution.reduceLeft( (s, x) => s + x + input.get(n)))._1
+			     (input.get, goal) :: labels
+		  	  })  
+		  	    	
+		  	  val qLabels = QLLabel[Double](labels)
+		  	  val qLearning = QLearning[Double](qConfig, qLabels, states.size)
+		   }
+		   case None => Display.error("Failed to extract all input data", logger)
+	   }
+	   * 
+	   */
+    }
+}
+
+
+// ------------------------------------  EOF ----------------------------------
